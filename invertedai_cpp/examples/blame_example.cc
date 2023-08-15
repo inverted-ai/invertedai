163cdc71d588875218322e85bb51efcf08e1900a
Ke Zhang <kezhang@pop-os.localdomain>
2023-08-14 17:52:48 -0700

    Remove unused code

--- invertedai_cpp/examples/blame_example.cc
+++ invertedai_cpp/examples/blame_example.cc
@@ -13,14 +13,9 @@
 using tcp = net::ip::tcp;    // from <boost/asio/ip/tcp.hpp>
 using json = nlohmann::json; // from <json.hpp>
 
-// usage: ./client $location $agent_num $timestep $api_key
+// usage: ./blame_example $api_key
 int main(int argc, char **argv) {
   try {
-/*
-    const std::string location(argv[1]);
-    const int agent_num = std::stoi(argv[2]);
-    const int timestep = std::stoi(argv[3]);
-*/
     const std::string api_key(argv[1]);
 
     net::io_context ioc;
@@ -34,56 +29,14 @@ int main(int argc, char **argv) {
     invertedai::BlameRequest blame_req(
         invertedai::read_file("examples/blame_body.json"));
 
-    std::cout << blame_req.body_str() << std::endl;
+    std::cout << "blame request: \n" << blame_req.body_str() << std::endl;
 
     // get response of location information
     invertedai::BlameResponse blame_res =
         invertedai::blame(blame_req, &session);
 
-    std::cout << blame_res.body_str() << std::endl;
+    std::cout << "blame response: \n" << blame_res.body_str() << std::endl;
 
-/*
-    // use opencv to decode and save the bird's eye view image of the simulation
-    auto image = cv::imdecode(loc_info_res.birdview_image(), cv::IMREAD_COLOR);
-    cv::cvtColor(image, image, cv::COLOR_BGR2RGB);
-    int frame_width = image.rows;
-    int frame_height = image.cols;
-    cv::VideoWriter video("iai-demo.avi",
-                          cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), 10,
-                          cv::Size(frame_width, frame_height));
-
-    // construct request for initializing the simulation (placing NPCs on the
-    // map)
-    invertedai::InitializeRequest init_req(
-        invertedai::read_file("examples/initialize_body.json"));
-    // set the location
-    init_req.set_location(location);
-    // set the number of agents
-    init_req.set_num_agents_to_spawn(agent_num);
-    // get the response of simulation initialization
-    invertedai::InitializeResponse init_res =
-        invertedai::initialize(init_req, &session);
-
-    // construct request for stepping the simulation (driving the NPCs)
-    invertedai::DriveRequest drive_req(
-        invertedai::read_file("examples/drive_body.json"));
-    drive_req.set_location(location);
-    drive_req.update(init_res);
-
-    for (int t = 0; t < timestep; t++) {
-      // step the simulation by driving the agents
-      invertedai::DriveResponse drive_res =
-          invertedai::drive(drive_req, &session);
-      // use opencv to decode and save the bird's eye view image of the
-      // simulation
-      auto image = cv::imdecode(drive_res.birdview(), cv::IMREAD_COLOR);
-      cv::cvtColor(image, image, cv::COLOR_BGR2RGB);
-      video.write(image);
-      drive_req.update(drive_res);
-      std::cout << "Remaining iterations: " << timestep - t << std::endl;
-    }
-    video.release();
-*/
   } catch (std::exception const &e) {
     std::cerr << "Error: " << e.what() << std::endl;
     return EXIT_FAILURE;
