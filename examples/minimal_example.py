259d1b32887ace07b56152b189fff0b6b17aa1b2
Kieran Ratcliffe <kieranratcliffeinvertedai@pop-os.localdomain>
2023-08-18 16:57:16 -0700

    Deleted some other unnecessary demos and updated the python demos to use ScenePlotter.

--- examples/minimal_example.py
+++ examples/minimal_example.py
@@ -1,11 +1,12 @@
 import numpy as np
-import imageio
+import matplotlib.pyplot as plt
 import invertedai as iai
 
 location = "iai:drake_street_and_pacific_blvd"  # select one of available locations
 
 iai.add_apikey('')  # specify your key here or through the IAI_API_KEY variable
 
+print("Begin initialization.")
 # get static information about a given location including map in osm
 # format and list traffic lights with their IDs and locations.
 location_info_response = iai.location_info(location=location)
@@ -22,7 +23,18 @@ response = iai.initialize(
 )
 agent_attributes = response.agent_attributes  # get dimension and other attributes of NPCs
 
-images = [response.birdview.decode()]  # images storing visualizations of subsequent states
+location_info_response = iai.location_info(location=location)
+rendered_static_map = location_info_response.birdview_image.decode()
+scene_plotter = iai.utils.ScenePlotter(rendered_static_map,
+                                       location_info_response.map_fov,
+                                       (location_info_response.map_center.x, location_info_response.map_center.y),
+                                       location_info_response.static_actors)
+scene_plotter.initialize_recording(
+    agent_states=response.agent_states,
+    agent_attributes=agent_attributes,
+)
+
+print("Begin stepping through simulation.")
 for _ in range(100):  # how many simulation steps to execute (10 steps is 1 second)
 
     # get next traffic light state
@@ -38,8 +50,18 @@ for _ in range(100):  # how many simulation steps to execute (10 steps is 1 seco
         traffic_lights_states=light_response.traffic_lights_states,
     )
 
-    # save the visualization - requires np and cv2
-    images.append(response.birdview.decode())
+    # save the visualization
+    scene_plotter.record_step(response.agent_states,light_response.traffic_lights_states)
 
+print("Simulation finished, save visualization.")
 # save the visualization to disk
-imageio.mimsave("iai-example.gif", np.array(images), format="GIF-PIL")
+fig, ax = plt.subplots(constrained_layout=True, figsize=(50, 50))
+gif_name = 'minimal_example.gif'
+scene_plotter.animate_scene(
+    output_name=gif_name,
+    ax=ax,
+    direction_vec=False,
+    velocity_vec=False,
+    plot_frame_number=True
+)
+print("Done")
