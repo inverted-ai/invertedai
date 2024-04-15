b1b95a362c928bb4d9e63b0a4e98b577e60e818e
Kieran Ratcliffe <kieranratcliffeinvertedai@pop-os.localdomain>
2024-04-15 16:25:11 -0700

    Added model version, seeds, and multiple simulations to area driving.

--- examples/large_map_example.py
+++ examples/large_map_example.py
@@ -9,85 +9,103 @@ import pygame
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 import time
+import random
 
 def main(args):
+    if args.model_version_drive == "None": 
+        model_version = None
+    else:
+        model_version = args.model_version_drive
+    for i in range(args.num_simulations):
+        try:
+            initialize_seed = random.randint(1,10000)
+            drive_seed = random.randint(1,10000)
 
-    map_center = tuple(args.map_center)
+            map_center = tuple(args.map_center)
 
-    print(f"Call location info.")
-    location_info_response = iai.location_info(
-        location = args.location,
-        rendering_fov = args.fov,
-        rendering_center = map_center
-    )
+            print(f"Call location info.")
+            location_info_response = iai.location_info(
+                location = args.location,
+                rendering_fov = args.fov,
+                rendering_center = map_center
+            )
 
-    print(f"Begin initialization.") 
-    initialize_response = iai.utils.area_initialization(
-        location = args.location, 
-        agent_density = args.agent_density, 
-        scaling_factor = 1.0,
-        width = int(args.width/2),
-        height = int(args.height/2),
-        map_center = map_center
-    )
+            print(f"Begin initialization.") 
+            initialize_response = iai.utils.area_initialization(
+                location = args.location, 
+                agent_density = args.agent_density, 
+                scaling_factor = 1.0,
+                width = int(args.width/2),
+                height = int(args.height/2),
+                map_center = map_center,
+                random_seed = initialize_seed
+            )
 
-    print(f"Set up simulation.")    
-    map_extent = max([args.width,args.height])
-    cfg = AreaDriverConfig(
-        location = args.location,
-        area_center = map_center,
-        area_fov = map_extent,
-        quadtree_capacity = args.capacity,
-        render_fov=args.fov,
-        pygame_window = args.display_sim,
-        show_quadtree = args.display_quadtree,
-        rendered_static_map = location_info_response.birdview_image.decode()
-    )
+            print(f"Set up simulation.")    
+            map_extent = max([args.width,args.height])
+            cfg = AreaDriverConfig(
+                location = args.location,
+                area_center = map_center,
+                area_fov = map_extent,
+                quadtree_capacity = args.capacity,
+                render_fov=args.fov,
+                pygame_window = args.display_sim,
+                show_quadtree = args.display_quadtree,
+                rendered_static_map = location_info_response.birdview_image.decode(),
+                random_seed = drive_seed,
+                model_version = model_version
+            )
 
-    simulation = AreaDriver(
-        cfg = cfg,
-        location_response = location_info_response,
-        initialize_response = initialize_response
-    )
+            simulation = AreaDriver(
+                cfg = cfg,
+                location_response = location_info_response,
+                initialize_response = initialize_response
+            )
 
-    if args.save_sim_gif:
-        rendered_static_map = location_info_response.birdview_image.decode()
-        scene_plotter = iai.utils.ScenePlotter(
-            rendered_static_map,
-            args.fov,
-            map_center,
-            location_info_response.static_actors,
-            resolution=(1080,1080),
-            dpi=200
-        )
-        scene_plotter.initialize_recording(
-            agent_states=initialize_response.agent_states,
-            agent_attributes=initialize_response.agent_attributes,
-        )
+            if args.save_sim_gif:
+                rendered_static_map = location_info_response.birdview_image.decode()
+                scene_plotter = iai.utils.ScenePlotter(
+                    rendered_static_map,
+                    args.fov,
+                    map_center,
+                    location_info_response.static_actors,
+                    resolution=(2048,2048),
+                    dpi=300
+                )
+                scene_plotter.initialize_recording(
+                    agent_states=initialize_response.agent_states,
+                    agent_attributes=initialize_response.agent_attributes,
+                    traffic_light_states=initialize_response.traffic_lights_states
+                )
 
-    total_num_agents = len(simulation.agent_states)
-    print(f"Number of agents in simulation: {total_num_agents}")
+            total_num_agents = len(simulation.agent_states)
+            print(f"Number of agents in simulation: {total_num_agents}")
 
-    print(f"Begin stepping through simulation.")
-    for _ in tqdm(range(args.sim_length)):
-        simulation.drive()
-        
-        if args.save_sim_gif: scene_plotter.record_step(simulation.agent_states,simulation.traffic_lights_states)
+            print(f"Begin stepping through simulation.")
+            for _ in tqdm(range(args.sim_length)):
+                simulation.drive()
+                
+                if args.save_sim_gif: scene_plotter.record_step(simulation.agent_states,simulation.traffic_lights_states)
 
+            if args.save_sim_gif:
+                print("Simulation finished, save visualization.")
+                # save the visualization to disk
+                fig, ax = plt.subplots(constrained_layout=True, figsize=(50, 50))
+                plt.axis('off')
+                current_time = int(time.time())
+                # gif_name = f'large_map_example_{int(time.time())}_{total_num_agents}agents.gif'
+                gif_name = f'large_map_example_{current_time}_location-{args.location.split(":")[-1]}_density-{args.agent_density}_center-x{map_center[0]}y{map_center[1]}_width-{args.width}_height-{args.height}_initseed-{initialize_seed}_driveseed-{drive_seed}_modelversion-{model_version}.gif'
+                scene_plotter.animate_scene(
+                    output_name=gif_name,
+                    ax=ax,
+                    direction_vec=False,
+                    velocity_vec=False,
+                    plot_frame_number=True,
+                )
+            print("Done")
 
-    if args.save_sim_gif:
-        print("Simulation finished, save visualization.")
-        # save the visualization to disk
-        fig, ax = plt.subplots(constrained_layout=True, figsize=(50, 50))
-        gif_name = f'large_map_example_{int(time.time())}_{total_num_agents}agents.gif'
-        scene_plotter.animate_scene(
-            output_name=gif_name,
-            ax=ax,
-            direction_vec=False,
-            velocity_vec=False,
-            plot_frame_number=True,
-        )
-    print("Done")
+        except Exception as e:
+            print(f"Error: {e}")
 
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser(description=__doc__)
@@ -159,6 +177,18 @@ if __name__ == '__main__':
         type=bool,
         help=f"If the in-simulation visualization is active, display the quadtree as well.",
         default=False
+    )
+    argparser.add_argument(
+        '--num-simulations',
+        type=int,
+        help=f"Number of simulations to run one after the other.",
+        default=1
+    )
+    argparser.add_argument(
+        '--model-version-drive',
+        type=str,
+        help=f"Version of the DRIVE model to use during the simulation.",
+        default="None"
     )
     args = argparser.parse_args()
 
