37aaae5a2963c3d3171bf1c7cd5bb2afee900aa4
KieranRatcliffeInvertedAI <kieran.ratcliffe@inverted.ai>
2025-01-08 11:00:18 -0800

    Added visualization feature to the debug logger tool.

--- invertedai/logs/debug_logger.py
+++ invertedai/logs/debug_logger.py
@@ -2,9 +2,13 @@ import logging
 import json
 import os
 
+import invertedai as iai
+from invertedai.common import AgentState, AgentProperties, TrafficLightState
+
 from collections import defaultdict
 from typing import List, Optional, Dict, Tuple
 from datetime import datetime, timezone
+import matplotlib.pyplot as plt
 
 logger = logging.getLogger(__name__)
 
@@ -76,3 +80,81 @@ class DebugLogger:
     def write_data_to_log(self):
         with open(self.log_path, "w") as outfile:
             json.dump(self.data, outfile)
+
+    @classmethod
+    def visualize_log(
+        cls,
+        log_data: Dict,
+        gif_name: str = "./debug_log_visualization.gif",
+        fov: int = 100,
+        map_center: Tuple[float] = None
+    ):
+        location_info_response = None
+        if "location_info_responses" in log_data:
+            if len(log_data["location_info_responses"]) > 0:
+                location_info_response = json.loads(log_data["location_info_responses"][-1])
+
+        if location_info_response is None:
+            if "initialize_requests" in log_data:
+                if len(log_data["initialize_requests"]) > 0:
+                    location_info_response = iai.location_info(
+                        location = json.loads(log_data["initialize_requests"][-1])["location"],
+                        rendering_fov = fov,
+                        rendering_center = map_center
+                    )
+                    map_center = tuple([location_info_response.map_center.x, location_info_response.map_center.y]) if map_center is None else map_center
+
+        if location_info_response is None:
+            raise Exception("No location data in the log to be able to visualize the data.")
+        rendered_static_map = location_info_response.birdview_image.decode()
+
+        scene_plotter = iai.utils.ScenePlotter(
+            map_image=rendered_static_map,
+            fov=fov,
+            xy_offset=map_center,
+            static_actors=location_info_response.static_actors,
+            resolution=(2048,2048),
+            dpi=300
+        )
+        scene_plotter.initialize_recording(
+            agent_states=[AgentState.fromlist(s) for s in json.loads(log_data["initialize_responses"][-1])["agent_states"]],
+            agent_properties=[AgentProperties(length=s["length"],width=s["width"],rear_axis_offset=s["rear_axis_offset"],agent_type=s["agent_type"],waypoint=s["waypoint"],max_speed=s["max_speed"]) for s in json.loads(log_data["initialize_responses"][-1])["agent_properties"]],
+            traffic_light_states=json.loads(log_data["initialize_responses"][-1])["traffic_lights_states"]
+        )
+
+        for response_json in log_data["drive_responses"]:
+            response = json.loads(response_json)
+            scene_plotter.record_step([AgentState.fromlist(s) for s in response["agent_states"]],response["traffic_lights_states"])
+
+        # save the visualization to disk
+        fig, ax = plt.subplots(constrained_layout=True, figsize=(50, 50))
+        plt.axis('off')
+        scene_plotter.animate_scene(
+            output_name=gif_name,
+            ax=ax,
+            direction_vec=False,
+            velocity_vec=False,
+            plot_frame_number=True,
+        )
+        plt.close(fig)
+
+    @classmethod
+    def read_log_from_path(
+        cls,
+        debug_log_path: str,
+        is_visualize_log: bool = False,
+        **kwargs
+    ):
+        with open(debug_log_path) as json_file:
+            log_data = json.load(json_file)
+
+            if is_visualize_log:
+                cls.visualize_log(
+                    log_data=log_data,
+                    **kwargs
+                )
+
+            return log_data
+
+
+    
