a5472c2cd92fbb4befbcce18611e0c1b4266803a
atao-inverted <anna.tao@inverted.ai>
2026-03-10 10:19:04 -0700

    fixed formating of function calls and added some documentation code

--- examples/simulation_manager_external_agent_example.py
+++ examples/simulation_manager_external_agent_example.py
@@ -24,26 +24,60 @@ if api_key is None:
 
 print("Begin initialization.")
 log_reader.initialize()
-location_info_response = iai.location_info(location=LOCATION, include_map_source=True)
-scene_plotter_cfg = ScenePlotterConfig(location=LOCATION, location_info_response=location_info_response)
+location_info_response = iai.location_info(
+    location=LOCATION, 
+    include_map_source=True
+)
+scene_plotter_cfg = ScenePlotterConfig(
+    location=LOCATION, 
+    location_info_response=location_info_response
+)
 waypoint_cfg = WaypointManagerConfig(lanelet_map = location_info_response.get_lanelet_map())
-log_cfg = LogWriterConfig(log_path="keyed_minimal_example_log.json",location=LOCATION, location_info_response=location_info_response)
-simulation_manager = SimulationManager(scene_plotter_cfg=scene_plotter_cfg, waypoint_cfg=waypoint_cfg, log_writer_cfg=log_cfg)
-regions = iai.get_regions_default(agent_count_dict = {AgentType.car: NUM_AGENTS}, location = LOCATION, map_center=tuple([location_info_response.map_center.x, location_info_response.map_center.y]))
+log_cfg = LogWriterConfig(
+    log_path="keyed_minimal_example_log.json",
+    location=LOCATION, 
+    location_info_response=location_info_response
+)
+simulation_manager = SimulationManager(
+    scene_plotter_cfg=scene_plotter_cfg, 
+    waypoint_cfg=waypoint_cfg, 
+    log_writer_cfg=log_cfg
+)
+regions = iai.get_regions_default(
+    agent_count_dict = {AgentType.car: NUM_AGENTS}, 
+    location = LOCATION, 
+    map_center=tuple([location_info_response.map_center.x, location_info_response.map_center.y])
+)
 external_agent_data = {
-    "new_agent": AgentData(
-        state=log_reader.agent_states[0], # add in the state of the external agent
-        properties=log_reader.agent_properties[0], # add in the properties of the external agent
-        recurrent=None # no recurrent state necessary for initialization
+    # please use our Scenario Builder to generate state and properties for your external agents: http://dashboard.inverted.ai/sandbox
+    "new": AgentData(
+        state=AgentState(
+            center=Point(x=-45.19154717515613, y=46.50373005906251),
+            orientation=4.7,
+            speed=1.0
+        ),
+        properties=AgentProperties(
+            agent_type=AgentType.car,
+            length=4.0,
+            width=2.02,
+            rear_axis_offset=1.784,
+        )
     )
 }
-response = simulation_manager.initialize(location=LOCATION, regions=regions, external_agent_data=external_agent_data)
+response = simulation_manager.initialize(
+    location=LOCATION, 
+    regions=regions, 
+    external_agent_data=external_agent_data
+)
 print("initialized agents with ids ", simulation_manager.get_agent_ids())
 rendered_static_map = location_info_response.birdview_image.decode()
 
 print("Begin stepping through simulation.")
 for step in range(SIM_LENGTH):
-    response = simulation_manager.drive(location=LOCATION, light_recurrent_states=response.light_recurrent_states)
+    response = simulation_manager.drive(
+        location=LOCATION, 
+        light_recurrent_states=response.light_recurrent_states
+    )
 
 print("Simulation finished, save visualization.")
 
