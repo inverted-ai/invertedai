a154b6386f81e875074a4a13731e8cb387e793cf
KieranRatcliffeInvertedAI <kieran.ratcliffe@inverted.ai>
2026-03-29 19:56:26 -0700

    Fixed missing comma

--- invertedai/helpers/simulation_manager.py
+++ invertedai/helpers/simulation_manager.py
@@ -59,7 +59,7 @@ class SimulationManager:
                     fov,
                     map_center,
                     scene_plotter_cfg.location_info_response.static_actors,
-                    left_hand_coordinates = scene_plotter_cfg.location.split(":")[0] == "carla"
+                    left_hand_coordinates = scene_plotter_cfg.location.split(":")[0] == "carla",
                     resolution=(2048,2048)
                 )
             self.agents_dict: SimulationAgentDict = defaultdict(AgentData)
