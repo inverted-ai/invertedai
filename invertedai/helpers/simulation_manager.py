e1aee7a85403ab3b803f516608cb6e9d811f729a
atao-inverted <anna.tao@inverted.ai>
2026-03-31 15:59:35 -0700

    revert scene plotter changes

--- invertedai/helpers/simulation_manager.py
+++ invertedai/helpers/simulation_manager.py
@@ -42,25 +42,12 @@ class SimulationManager:
             self.scene_plotter = None
             self.scene_plotter_cfg = scene_plotter_cfg
             if scene_plotter_cfg:
-                map_center = scene_plotter_cfg.map_center if scene_plotter_cfg.map_center is not None else \
-                    (scene_plotter_cfg.location_info_response.map_center.x, scene_plotter_cfg.location_info_response.map_center.y)
-                fov = scene_plotter_cfg.fov if scene_plotter_cfg.fov is not None else \
-                    scene_plotter_cfg.location_info_response.map_fov
-                birdview_image = scene_plotter_cfg.location_info_response.birdview_image.decode()
-                if scene_plotter_cfg.fov is not None or scene_plotter_cfg.map_center is not None:
-                    correct_li = location_info(
-                        location=scene_plotter_cfg.location,
-                        rendering_fov=fov,
-                        rendering_center=map_center,
-                    )
-                    birdview_image = correct_li.birdview_image.decode()
                 self.scene_plotter = ScenePlotter(
-                    birdview_image,
-                    fov,
-                    map_center,
+                    scene_plotter_cfg.location_info_response.birdview_image.decode(),
+                    scene_plotter_cfg.location_info_response.map_fov,
+                    (scene_plotter_cfg.location_info_response.map_center.x, scene_plotter_cfg.location_info_response.map_center.y),
                     scene_plotter_cfg.location_info_response.static_actors,
-                    left_hand_coordinates = scene_plotter_cfg.location.split(":")[0] == "carla",
-                    resolution=(2048,2048)
+                    left_hand_coordinates = scene_plotter_cfg.location.split(":")[0] == "carla"
                 )
             self.agents_dict: SimulationAgentDict = defaultdict(AgentData)
             self.waypoint_manager: Optional[WaypointManager] = None
